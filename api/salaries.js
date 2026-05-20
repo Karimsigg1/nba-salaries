b24b0ec765fae5362870ecbea7b0a225f36a4d82
Karim Sigg <Karimsigg@gmail.com>
2026-05-20 16:16:20 -0400

    simplify API

--- api/salaries.js
+++ api/salaries.js
@@ -3,20 +3,13 @@ export default async function handler(req, res) {
   try {
     const response = await fetch('https://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams?limit=32');
     const data = await response.json();
-    const teams = await Promise.all(
-      data.sports[0].leagues[0].teams.map(async (t) => {
-        const team = t.team;
-        const rosterRes = await fetch(`https://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams/${team.id}/roster`);
-        const rosterData = await rosterRes.json();
-        const athletes = rosterData.athletes || [];
-        const players = athletes.flatMap(g => g.items || []).map(p => ({
-          name: p.displayName,
-          position: p.position?.abbreviation || 'N/A',
-          jersey: p.jersey || '-',
-        }));
-        return { team: team.displayName, abbreviation: team.abbreviation, logo: team.logos?.[0]?.href || '', players };
-      })
-    );
+    const teams = data.sports[0].leagues[0].teams.map(t => ({
+      team: t.team.displayName,
+      abbreviation: t.team.abbreviation,
+      logo: t.team.logos?.[0]?.href || '',
+      color: t.team.color || '1a1f2e',
+      players: []
+    }));
     res.status(200).json({ teams });
   } catch (err) {
     res.status(500).json({ error: err.message });
